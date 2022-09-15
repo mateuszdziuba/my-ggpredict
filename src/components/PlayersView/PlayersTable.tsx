2f50104a239be9adddf7c3f35fc3d0d7fc2be307
mateusz <mateusz@blazar.localdomain>
2022-09-15 16:02:00 +0200

    Setting table to fixed

--- src/components/PlayersView/PlayersTable.tsx
+++ src/components/PlayersView/PlayersTable.tsx
@@ -25,17 +25,17 @@ const PlayersTable = ({ players }: PlayersTablesProps) => {
 
   return (
     <div className="max-h-[618px] w-full overflow-auto">
-      <table className="table table-auto w-full">
+      <table className="table table-fixed w-full">
         <thead className="sticky top-0 z-10 text-gray-500 border-b bg-inherit border-white/10">
           <tr className="[&>*]:bg-[#181c2c] ">
-            <th className="rounded-none"> </th>
-            <th className="w-[400px]">Name</th>
-            <th>Nick</th>
-            <th>Age</th>
-            <th>Country</th>
-            <th className="w-[1px] text-green-500">Won</th>
-            <th className="w-[1px]">Drawn</th>
-            <th className="w-1[px] text-red-600">Lost</th>
+            <th className="w-[2%] rounded-none"> </th>
+            <th className="w-1/4">Name</th>
+            <th className="w-1/6">Nick</th>
+            <th className="w-1/6">Age</th>
+            <th className="w-1/6">Country</th>
+            <th className="w-[4%] text-green-500">Won</th>
+            <th className="w-[4%]">Drawn</th>
+            <th className="w-[4%] text-red-600">Lost</th>
           </tr>
         </thead>
         <tbody className="bg-transparent align-baseline">
@@ -47,7 +47,7 @@ const PlayersTable = ({ players }: PlayersTablesProps) => {
                   key={player.id}
                   className="text-white [&>*]:bg-transparent [&>*]:border-white/10"
                 >
-                  <td className="relative z-0 w-5 p-0 after:absolute after:right-0 after:top-1/2 after:w-[1px] after:h-2/3 after:translate-y-[-50%] after:bg-white/10">
+                  <td className="relative z-0 p-0 after:absolute after:right-0 after:top-1/2 after:w-[1px] after:h-2/3 after:translate-y-[-50%] after:bg-white/10">
                     <img src={logoWhite} className="w-5 h-5" />
                   </td>
                   <td>{player.name}</td>
@@ -58,15 +58,9 @@ const PlayersTable = ({ players }: PlayersTablesProps) => {
                       className={`z-0 fi fi-${player.country.toLowerCase()}`}
                     ></span>
                   </td>
-                  <td className="w-[1px] text-center">
-                    {playerResults?.W || 0}
-                  </td>
-                  <td className="w-[1px] text-center">
-                    {playerResults?.D || 0}
-                  </td>
-                  <td className="w-[1px] text-center">
-                    {playerResults?.L || 0}
-                  </td>
+                  <td className="text-center">{playerResults?.W || 0}</td>
+                  <td className="text-center">{playerResults?.D || 0}</td>
+                  <td className="text-center">{playerResults?.L || 0}</td>
                 </tr>
               );
             })
